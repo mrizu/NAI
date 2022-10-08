53bbf3861c2b8b2ee47397045232581085b83846
karol <karol0704@wp.pl>
2022-10-08 13:39:17 +0200

    finished

--- Zajecia 1/lab1.cpp
+++ Zajecia 1/lab1.cpp
@@ -1,4 +1,3 @@
-#include <any>
 #include <functional>
 #include <iostream>
 #include <map>
@@ -6,55 +5,32 @@
 #include <vector>
 #include <cmath>
 
+using chosen_function = std::function<double(std::vector<double>)>;
+
+void outputResult(std::vector<double> numbers, chosen_function func){
+    using namespace std;
+    cout << func(numbers);
+}
+
 int main(int argc, char **argv) {
     using namespace std;
-    map<string, function<double(vector<double>)>> my_functions;
-    my_functions["add"] = [](vector<double> x) {return x.at(0) + x.at(1);};
-    my_functions["sin"] = [](vector<double> x) {return sin(x.at(0));};
-    my_functions["mod"] = [](vector<double> x) {return x.at(0) % x.at(1);};
+    map<string, chosen_function> math_func;
+    math_func["add"] = [](vector<double> x) {return x.at(0) + x.at(1);};
+    math_func["sin"] = [](vector<double> x) {return sin(x.at(0));};
+    math_func["mod"] = [](vector<double> x) {return (int)x.at(0) % (int)x.at(1);};
 
     try {
-        vector<string> argumenty(argv, argv + argc);
-        auto selected_f = argumenty.at(1);
-        cout << argc << " " << argv << endl;
+        vector<string> arguments(argv, argv + argc);
+        vector<double> func_arguments;
+        for(int i = 2;i < arguments.size();i++){
+            func_arguments.push_back(stod(arguments.at(i)));
+        }
+        auto selected_f = arguments.at(1);
+        outputResult(func_arguments, math_func.at(selected_f));
     } catch (std::out_of_range aor) {
-        cout << "test";
+        cout << "Please enter parameters in format: [FUNCTION NAME] [ARGUMENT] ..." << endl;
+        cout << "Available functions: add, sin, modw" << endl;
     }
 
     return 0;
 }
-
-
-
-//#include <any>
-//#include <functional>
-//#include <iostream>
-//#include <map>
-//#include <string>
-//#include <vector>
-//using mojamapa_t = std::map<std::string, std::string>;
-//using mojafunkcja_t = std::function<std::string(std::string)>;
-//void wypisz(mojamapa_t mapa, mojafunkcja_t fun) {
-//    using namespace std;
-//    for (auto kv : mapa) {
-//        auto [k, v] = kv;
-//        cout << "klucz: " << k << "; wartosc " << fun(v) << endl;
-//    }
-//}
-//int main(int argc, char **argv) {
-//    using namespace std;
-//    map<string, string> mapa = {{"imie", "Jan"}};
-//    map<string, mojafunkcja_t> formatery;
-//    formatery["r"] = [](string x) { return "== " + x + " =="; };
-//    formatery["p"] = [](string x) { return "__" + x + "__"; };
-//    try {
-//        vector<string> argumenty(argv, argv + argc);
-//        auto selected_f = argumenty.at(1);
-//        wypisz(mapa, formatery.at(selected_f));
-//    } catch (std::out_of_range aor) {
-//        cout << "podaj argument. Dostępne to: ";
-//        for (auto [k, v] : formatery) cout << " " << k;
-//        cout << endl;
-//    }
-//    return 0;
-//}
