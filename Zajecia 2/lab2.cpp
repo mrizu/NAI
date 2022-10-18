e89a248b1511c5a7f72b3451d99ef2ac880de245
karol <karol0704@wp.pl>
2022-10-18 13:59:28 +0200

    lab2 finished

--- Zajecia 2/lab2.cpp
+++ Zajecia 2/lab2.cpp
@@ -1,58 +1,63 @@
 #include <iostream>
 #include <vector>
-#include <functional>
 #include <random>
 
-/**
- * domain - generate domain points. Throws exception when all the points were returned
- */
-auto brute_force = [](auto f, auto domain) {
-    auto current_p = domain();
-    auto best_point = current_p;
-    try {
-        while (true) {
-            if (f(current_p) < f(best_point)) {
-                best_point = current_p;
-            }
-            current_p = domain();
-        }
-    } catch (std::exception &e) {
-    }
-    return best_point;
-};
-using domain_t = std::vector<double>;
 std::random_device rd;
 std::mt19937 mt_generator(rd());
-domain_t hill_climbing(const std::function<double(domain_t)> &f, domain_t minimal_d, domain_t maximal_d, int max_iterations) {
-    domain_t current_p(minimal_d.size());
-    for (int i = 0; i < minimal_d.size(); i++) {
-        std::uniform_real_distribution<double> dist(minimal_d[i], maximal_d[i]);
-        current_p[i] = dist(mt_generator);
-    }
-    for (int iteration = 0; iteration < max_iterations; iteration++) {
-        domain_t new_p(minimal_d.size());
-        for (int i = 0; i < minimal_d.size(); i++) {
-            std::uniform_real_distribution<double> dist(-1.0/128.0, 1.0/128.0);
-            new_p[i] = current_p[i] + dist(mt_generator);
+
+auto brute_force = [](auto f, auto domain, int iterations, double r1, double r2) {
+    auto current_p = domain(r1, r2);
+    auto best_point = current_p;
+
+    for (int i = 0; i < iterations; ++i) {
+        if (f(current_p) < f(best_point)) {
+            best_point = current_p;
         }
-        if (f(current_p) > f(new_p)) {
-            current_p = new_p;
-        }
+        current_p = domain(r1, r2);
     }
-    return current_p;
+
+    return best_point;
+};
+
+auto coords_generator(double r1, double r2) {
+    std::uniform_real_distribution<> coords(r1, r2);
+    return std::pair<double, double>(coords(mt_generator), coords(mt_generator));
 }
+
 int main() {
-    auto sphere_f = [](double x) {return x*x;};
-    double current_sphere_x = -10;
-    auto sphere_generator = [&]() {
-        current_sphere_x+= 1.0/128.0;
-        if (current_sphere_x >= 10) throw std::invalid_argument("finished");
-        return current_sphere_x;
+    auto beale_f = [](std::pair<double, double> pair) {
+        return pow(1.5 - pair.first + pair.first * pair.second, 2) +
+               pow(2.25 - pair.first + pair.first * pow(pair.second, 2), 2) +
+               pow(2.625 - pair.first + pair.first * pow(pair.second, 3), 2);
     };
-    auto best_point = brute_force(sphere_f, sphere_generator);
-    std::cout << "best x = " << best_point << std::endl;
-    auto sphere_f_v = [](domain_t x) {return x[0]*x[0];};
-    auto best2 = hill_climbing(sphere_f_v, {-10},{10},10000);
-    std::cout << "best x = " << best2[0] << std::endl;
+
+    auto booth_f = [](std::pair<double, double> pair) {
+        return pow((pair.first + 2 * pair.second - 7), 2) +
+               pow((2 * pair.first + pair.second - 5), 2);
+    };
+
+    auto matyas_f = [](std::pair<double, double> pair) {
+        return 0.26 * (pow(pair.first, 2) +
+                       pow(pair.second, 2) - 0.48 * pair.first * pair.second);
+    };
+    std::cout << "-------------------------------------------------------------------" << std::endl;
+    auto best_beale = brute_force(beale_f, coords_generator, 2000000, -4.5, 4.5);
+    std::cout << "Best Beale x = " << best_beale.first << " y = " << best_beale.second
+              << " | result: " << beale_f(best_beale)
+              << std::endl;
+
+    std::cout << "-------------------------------------------------------------------" << std::endl;
+    auto best_booth = brute_force(booth_f, coords_generator, 2000000, -10, 10);
+    std::cout << "Best Booth x = " << best_booth.first << " y = " << best_booth.second
+              << " | result: " << booth_f(best_booth)
+              << std::endl;
+
+    std::cout << "-------------------------------------------------------------------" << std::endl;
+    auto best_matyas = brute_force(matyas_f, coords_generator, 2000000, -10, 10);
+    std::cout << "Best Matyas x = " << best_matyas.first << " y = " << best_matyas.second
+              << " | result: " << matyas_f(best_matyas)
+              << std::endl;
+    std::cout << "-------------------------------------------------------------------" << std::endl;
+
     return 0;
 }
