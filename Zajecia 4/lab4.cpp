cd2b152fcea1c538f60ee0a3d2d7f015b9747303
karol <karol0704@wp.pl>
2022-11-06 16:22:58 +0100

    lab4 fix

--- Zajecia 4/lab4.cpp
+++ Zajecia 4/lab4.cpp
@@ -2,27 +2,27 @@
 #include <vector>
 #include <random>
 
-using genotype_t = std::vector<int>;
-using chromosome_t = std::vector<genotype_t>;
+using chromosome_t = std::vector<int>;
+using genotype_t = std::vector<chromosome_t>;
 std::random_device rd;
 std::mt19937 mt_generator(rd());
 
-std::pair<double, double> decode(genotype_t genotype) {
+std::pair<double, double> decode(chromosome_t chromosome) {
     double x = 0.0;
     double y = 0.0;
     double base_x = 1.0;
     double base_y = 1.0;
-    int half_c = genotype.size() / 2;
+    int half_c = chromosome.size() / 2;
 
     for (int i = 0; i < half_c; i++) {
-        if (genotype[i] == 1){
+        if (chromosome[i] == 1){
             x += base_x;
         }
         base_x = base_x * 2;
     }
 
-    for (int i = half_c; i < genotype.size(); i++) {
-        if (genotype[i] == 1){
+    for (int i = half_c; i < chromosome.size(); i++) {
+        if (chromosome[i] == 1){
             y += base_y;
         }
         base_y = base_y * 2;
@@ -33,17 +33,17 @@ std::pair<double, double> decode(genotype_t genotype) {
     return {x, y};
 }
 
-chromosome_t chromosome_generator(int size) {
-    chromosome_t chromosome;
+genotype_t genotype_generator(int size) {
+    genotype_t genotype;
     for (int i = 0; i < size; i++){
-        genotype_t genotype;
+        chromosome_t chromosome;
         for (int j = 0; j < 100; j++){
             std::uniform_int_distribution<int> byte(0, 1);
-            genotype.push_back(byte(mt_generator));
+            chromosome.push_back(byte(mt_generator));
         }
-        chromosome.push_back(genotype);
+        genotype.push_back(chromosome);
     }
-    return chromosome;
+    return genotype;
 }
 
 auto ackley_f = [](std::pair<double, double> pair) {
@@ -51,17 +51,17 @@ auto ackley_f = [](std::pair<double, double> pair) {
            exp(0.5 * (cos(2 * M_PI * pair.first) + cos(2 * M_PI * pair.second))) + exp(1) + 20;
 };
 
-double fitness_function(genotype_t &genotype) {
-    return 1.0 / ackley_f(decode(genotype));
+double fitness_function(chromosome_t &chromosome) {
+    return 1.0 / ackley_f(decode(chromosome));
 }
 
 int main() {
     using namespace std;
-    chromosome_t chromosome = chromosome_generator(100 + (23366 % 10) * 2);
+    genotype_t genotype = genotype_generator(100 + (23366 % 10) * 2);
 
-    for (genotype_t &genotype: chromosome) {
-        pair<double, double> decoded = decode(genotype);
-       cout << "x: "<< decoded.first << " y: "<<  decoded.second << " | fitness: " << fitness_function(genotype) << endl;
+    for (chromosome_t &chromosome: genotype) {
+        pair<double, double> decoded = decode(chromosome);
+       cout << "x: "<< decoded.first << " y: "<<  decoded.second << " | fitness: " << fitness_function(chromosome) << endl;
     }
 
     return 0;
